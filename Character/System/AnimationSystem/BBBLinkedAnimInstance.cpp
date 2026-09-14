e8afa4d288e0eb833eda50c4921643744e3f81a5
bunkerboy <172847460@qq.com>
2026-09-14 10:01:44 +0800

    Expose thread safe linked animation accessors

--- Character/System/AnimationSystem/BBBLinkedAnimInstance.cpp
+++ Character/System/AnimationSystem/BBBLinkedAnimInstance.cpp
@@ -1,8 +1,437 @@
 #include "BBBWork/UBBBNexus/Character/System/AnimationSystem/BBBLinkedAnimInstance.h"
 
 #include "BBBWork/UBBBNexus/Character/System/AnimationSystem/BBBAnimInstance.h"
+#include "BBBWork/UBBBNexus/Equipment/Base/BBBEquipmentInstance.h"
 
 UBBBAnimInstance *UBBBLinkedAnimInstance::GetBBBMainAnimInstanceThreadSafe() const
 {
     return Cast<UBBBAnimInstance>(Blueprint_GetMainAnimInstance());
+}
+
+//------------------------------------------------------------------------------
+
+EBBBCharacterGait UBBBLinkedAnimInstance::GetLinkedGait() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return EBBBCharacterGait::Run;
+    }
+
+    return MainAnimInstance->GetGait();
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::IsLinkedWalking() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->IsWalking();
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::IsLinkedRunning() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->IsRunning();
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::IsLinkedSprinting() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->IsSprinting();
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::IsLinkedAiming() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->IsAiming();
+}
+
+//------------------------------------------------------------------------------
+
+float UBBBLinkedAnimInstance::GetLinkedAimIntentAlpha() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return 0.0f;
+    }
+
+    return MainAnimInstance->GetAimIntentAlpha();
+}
+
+//------------------------------------------------------------------------------
+
+float UBBBLinkedAnimInstance::GetLinkedAimIKAlpha() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return 0.0f;
+    }
+
+    return MainAnimInstance->GetAimIKAlpha();
+}
+
+//------------------------------------------------------------------------------
+
+FVector UBBBLinkedAnimInstance::GetLinkedAimTargetComponentSpace() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return FVector::ZeroVector;
+    }
+
+    return MainAnimInstance->GetAimTargetComponentSpace();
+}
+
+//------------------------------------------------------------------------------
+
+FTransform UBBBLinkedAnimInstance::GetLinkedAimSourceLocalTransform() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return FTransform::Identity;
+    }
+
+    return MainAnimInstance->GetAimSourceLocalTransform();
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::HasLinkedValidAimTarget() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->HasValidAimTarget();
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::HasLinkedValidAimSource() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->HasValidAimSource();
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::HasLinkedMainHandEquipment() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->HasMainHandEquipment();
+}
+
+//------------------------------------------------------------------------------
+
+UBBBEquipmentInstance *UBBBLinkedAnimInstance::GetLinkedMainHandEquipmentInstance() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return nullptr;
+    }
+
+    return MainAnimInstance->GetMainHandEquipmentInstance();
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::IsLinkedReloading() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->IsReloading();
+}
+
+//------------------------------------------------------------------------------
+
+float UBBBLinkedAnimInstance::GetLinkedTimeSinceLastFire() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return BIG_NUMBER;
+    }
+
+    return MainAnimInstance->GetTimeSinceLastFire();
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::ShouldLinkedRaiseWeaponAfterFiring(const float Duration) const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->ShouldRaiseWeaponAfterFiring(Duration);
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::HasLinkedLeftHandIKOffsetRightHand() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    UBBBEquipmentInstance *EquipmentInstance = MainAnimInstance->GetMainHandEquipmentInstance();
+    if (!EquipmentInstance)
+    {
+        return false;
+    }
+
+    return EquipmentInstance->HasValidLeftHandIKOffset();
+}
+
+//------------------------------------------------------------------------------
+
+FVector UBBBLinkedAnimInstance::GetLinkedLeftHandIKOffsetRightHand() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return FVector::ZeroVector;
+    }
+
+    UBBBEquipmentInstance *EquipmentInstance = MainAnimInstance->GetMainHandEquipmentInstance();
+    if (!EquipmentInstance)
+    {
+        return FVector::ZeroVector;
+    }
+
+    return EquipmentInstance->GetLeftHandIKOffsetRightHand();
+}
+
+//------------------------------------------------------------------------------
+
+FVector UBBBLinkedAnimInstance::GetLinkedSourceActorLocation() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return FVector::ZeroVector;
+    }
+
+    return MainAnimInstance->SourceActorLocation;
+}
+
+//------------------------------------------------------------------------------
+
+FRotator UBBBLinkedAnimInstance::GetLinkedSourceActorRotation() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return FRotator::ZeroRotator;
+    }
+
+    return MainAnimInstance->SourceActorRotation;
+}
+
+//------------------------------------------------------------------------------
+
+FVector UBBBLinkedAnimInstance::GetLinkedSourceVelocity() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return FVector::ZeroVector;
+    }
+
+    return MainAnimInstance->SourceVelocity;
+}
+
+//------------------------------------------------------------------------------
+
+FVector UBBBLinkedAnimInstance::GetLinkedSourceLastUpdateVelocity() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return FVector::ZeroVector;
+    }
+
+    return MainAnimInstance->SourceLastUpdateVelocity;
+}
+
+//------------------------------------------------------------------------------
+
+FVector UBBBLinkedAnimInstance::GetLinkedSourceAcceleration() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return FVector::ZeroVector;
+    }
+
+    return MainAnimInstance->SourceAcceleration;
+}
+
+//------------------------------------------------------------------------------
+
+float UBBBLinkedAnimInstance::GetLinkedSourceGroundFriction() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return 0.0f;
+    }
+
+    return MainAnimInstance->SourceGroundFriction;
+}
+
+//------------------------------------------------------------------------------
+
+float UBBBLinkedAnimInstance::GetLinkedSourceBrakingFriction() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return 0.0f;
+    }
+
+    return MainAnimInstance->SourceBrakingFriction;
+}
+
+//------------------------------------------------------------------------------
+
+float UBBBLinkedAnimInstance::GetLinkedSourceBrakingFrictionFactor() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return 0.0f;
+    }
+
+    return MainAnimInstance->SourceBrakingFrictionFactor;
+}
+
+//------------------------------------------------------------------------------
+
+float UBBBLinkedAnimInstance::GetLinkedSourceBrakingDecelerationWalking() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return 0.0f;
+    }
+
+    return MainAnimInstance->SourceBrakingDecelerationWalking;
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::HasLinkedSeparateBrakingFriction() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->bSourceUseSeparateBrakingFriction;
+}
+
+//------------------------------------------------------------------------------
+
+float UBBBLinkedAnimInstance::GetLinkedSourceGravityZ() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return 0.0f;
+    }
+
+    return MainAnimInstance->SourceGravityZ;
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::IsLinkedMovingOnGround() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->bSourceMovingOnGround;
+}
+
+//------------------------------------------------------------------------------
+
+bool UBBBLinkedAnimInstance::IsLinkedCrouching() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return false;
+    }
+
+    return MainAnimInstance->bSourceCrouching;
+}
+
+//------------------------------------------------------------------------------
+
+float UBBBLinkedAnimInstance::GetLinkedGroundDistance() const
+{
+    const UBBBAnimInstance *MainAnimInstance = GetBBBMainAnimInstanceThreadSafe();
+    if (!MainAnimInstance)
+    {
+        return 0.0f;
+    }
+
+    return MainAnimInstance->GroundDistance;
 }
--- Character/System/AnimationSystem/BBBLinkedAnimInstance.h
+++ Character/System/AnimationSystem/BBBLinkedAnimInstance.h
@@ -2,9 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "BBBWork/UBBBNexus/Character/System/LocomotionSystem/Definition/BBBCharacterLocomotionRuntimeData.h"
 #include "BBBLinkedAnimInstance.generated.h"
 
 class UBBBAnimInstance;
+class UBBBEquipmentInstance;
 
 /** 链接动画层访问主动画实例的线程安全基类 */
 UCLASS(Abstract)
@@ -16,4 +18,136 @@ public:
     /** @return 当前骨骼网格体上的 BBB 主动画实例 */
     UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
     UBBBAnimInstance *GetBBBMainAnimInstanceThreadSafe() const;
+
+    /** @return 链接层读取的当前步态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Locomotion", meta = (BlueprintThreadSafe))
+    EBBBCharacterGait GetLinkedGait() const;
+
+    /** @return 链接层读取的行走状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Locomotion", meta = (BlueprintThreadSafe))
+    bool IsLinkedWalking() const;
+
+    /** @return 链接层读取的奔跑状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Locomotion", meta = (BlueprintThreadSafe))
+    bool IsLinkedRunning() const;
+
+    /** @return 链接层读取的冲刺状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Locomotion", meta = (BlueprintThreadSafe))
+    bool IsLinkedSprinting() const;
+
+    /** @return 链接层读取的瞄准状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Aim", meta = (BlueprintThreadSafe))
+    bool IsLinkedAiming() const;
+
+    /** @return 链接层读取的瞄准意图权重 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Aim", meta = (BlueprintThreadSafe))
+    float GetLinkedAimIntentAlpha() const;
+
+    /** @return 链接层读取的瞄准 IK 权重 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Aim", meta = (BlueprintThreadSafe))
+    float GetLinkedAimIKAlpha() const;
+
+    /** @return 链接层读取的组件空间瞄准目标 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Aim", meta = (BlueprintThreadSafe))
+    FVector GetLinkedAimTargetComponentSpace() const;
+
+    /** @return 链接层读取的瞄准来源本地变换 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Aim", meta = (BlueprintThreadSafe))
+    FTransform GetLinkedAimSourceLocalTransform() const;
+
+    /** @return 链接层读取的瞄准目标有效状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Aim", meta = (BlueprintThreadSafe))
+    bool HasLinkedValidAimTarget() const;
+
+    /** @return 链接层读取的瞄准来源有效状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Aim", meta = (BlueprintThreadSafe))
+    bool HasLinkedValidAimSource() const;
+
+    /** @return 链接层读取的主手装备状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Equipment", meta = (BlueprintThreadSafe))
+    bool HasLinkedMainHandEquipment() const;
+
+    /** @return 链接层读取的主手装备实例 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Equipment", meta = (BlueprintThreadSafe))
+    UBBBEquipmentInstance *GetLinkedMainHandEquipmentInstance() const;
+
+    /** @return 链接层读取的换弹状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Equipment", meta = (BlueprintThreadSafe))
+    bool IsLinkedReloading() const;
+
+    /** @return 链接层读取的上次开火时间 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Equipment", meta = (BlueprintThreadSafe))
+    float GetLinkedTimeSinceLastFire() const;
+
+    /**
+     * 判断链接层读取的最近一次开火是否仍应维持持枪姿势
+     * @param Duration\t持枪姿势维持时长
+     * @return 是否仍处于指定维持时间内
+     */
+    UFUNCTION(BlueprintPure, Category = "BBB|Equipment", meta = (BlueprintThreadSafe))
+    bool ShouldLinkedRaiseWeaponAfterFiring(float Duration) const;
+
+    /** @return 链接层读取的左手 IK 偏移是否有效 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Equipment", meta = (BlueprintThreadSafe))
+    bool HasLinkedLeftHandIKOffsetRightHand() const;
+
+    /** @return 链接层读取的相对右手骨骼的左手 IK 偏移 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Equipment", meta = (BlueprintThreadSafe))
+    FVector GetLinkedLeftHandIKOffsetRightHand() const;
+
+    /** @return 链接层读取的角色世界位置 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    FVector GetLinkedSourceActorLocation() const;
+
+    /** @return 链接层读取的角色世界旋转 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    FRotator GetLinkedSourceActorRotation() const;
+
+    /** @return 链接层读取的角色世界速度 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    FVector GetLinkedSourceVelocity() const;
+
+    /** @return 链接层读取的上次更新世界速度 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    FVector GetLinkedSourceLastUpdateVelocity() const;
+
+    /** @return 链接层读取的角色世界加速度 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    FVector GetLinkedSourceAcceleration() const;
+
+    /** @return 链接层读取的地面摩擦力 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    float GetLinkedSourceGroundFriction() const;
+
+    /** @return 链接层读取的制动摩擦力 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    float GetLinkedSourceBrakingFriction() const;
+
+    /** @return 链接层读取的制动摩擦系数 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    float GetLinkedSourceBrakingFrictionFactor() const;
+
+    /** @return 链接层读取的行走制动减速度 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    float GetLinkedSourceBrakingDecelerationWalking() const;
+
+    /** @return 链接层读取的独立制动摩擦力开关 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    bool HasLinkedSeparateBrakingFriction() const;
+
+    /** @return 链接层读取的重力加速度 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    float GetLinkedSourceGravityZ() const;
+
+    /** @return 链接层读取的地面移动状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    bool IsLinkedMovingOnGround() const;
+
+    /** @return 链接层读取的蹲伏状态 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    bool IsLinkedCrouching() const;
+
+    /** @return 链接层读取的胶囊体底部到地面距离 */
+    UFUNCTION(BlueprintPure, Category = "BBB|Animation Facts", meta = (BlueprintThreadSafe))
+    float GetLinkedGroundDistance() const;
 };
